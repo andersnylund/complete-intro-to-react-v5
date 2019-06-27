13fa6741e21ec33c9093e2ff60668bee68ed0a4b
Anders Nylund <anders@andersnylund.com>
2019-06-27 11:13:00 +0300

    Complete template literals

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt me!</Link>
-    <span aria-label="logo" role="img">
-      🐩
-    </span>
-  </header>
-);
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+
+  return (
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: pink;
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+        `}
+        aria-label="logo"
+        role="img"
+      >
+        🐩
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
