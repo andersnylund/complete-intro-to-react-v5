2d331275d5e8967bb33cf24f2ed3735505f4393c
Anders Nylund <anders@andersnylund.com>
2019-06-27 11:22:24 +0300

    Complete animation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
+
+import colors from "./colors";
+
+const spin = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`;
 
 const NavBar = () => {
-  const [padding, setPadding] = useState(15);
-
   return (
     <header
-      onClick={() => setPadding(padding + 15)}
       css={css`
-        background-color: pink;
-        padding: ${padding}px;
+        background-color: ${colors.primary};
+        padding: 15px;
       `}
     >
       <Link to="/">Adopt me!</Link>
       <span
         css={css`
           font-size: 60px;
+          &:hover {
+            text-decoration: underline;
+            animation: 1s ${spin} linear infinite reverse;
+          }
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
         `}
         aria-label="logo"
         role="img"
